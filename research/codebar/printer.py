403918a499dc52c4c25fa84299acfa828240d242
SuperZooper3 <54122151+SuperZooper3@users.noreply.github.com>
2022-09-19 21:30:55 -0400

    Printer with new data standard

--- research/codebar/printer.py
+++ research/codebar/printer.py
@@ -1,15 +1,40 @@
 # This printer script takes in a binary list of digits and turns them into a space (0) or a bar (1) saved into a file called printed.png
 from encode import encode
 from PIL import Image
+from math import ceil, log2
 
 digit_width = 16 # Pixel width of each digit
 digit_height = 200 # Pixel height of each digit
 
-def codePrint(digits, filename):
-    # for each digit, make digit_width copies of it in the array. Ex: [1,0] becomes [1,1,1,0,0,0]
+typeIndicatorBits = {
+    "num": [0,0],
+    "ascii":[0,1],
+    "utf8":[1,0],
+    "raw":[1,1],
+}
+
+def codePrint(digits, filename, type = "ascii"):
     horizontal_pixels = []
-    digits = [1,0,1] + digits + [1,0,1]
-    for digit in digits:
+
+    # Pad digits to the left to reach the closest byte
+    digits = [0] * ((8 - len(digits)) % 8) + digits
+
+    # Make sure the data will fit inside the max size
+    assert(len(digits)//8 < 2**8)
+
+    digitsLengthBits = [int(c) for c in "{:08b}".format(ceil(len(digits)/8))]
+
+    # count the number of "1" bars in the data
+    checksumValue = int(digits.count(1) % digits.count(0)) % ceil(len(digits)/8)
+    
+    checksumBits = [int(c) for c in "{0:b}".format(checksumValue)]
+
+    fullData = [1,0,1,1] + typeIndicatorBits[type] + digitsLengthBits + checksumBits + digits + [0,1,0,1]
+
+    print("Type bits: ", typeIndicatorBits[type], "Length bits: ", digitsLengthBits, "Checksum bits: ", checksumBits, "Digits: ", digits)
+
+    # for each digit, make digit_width copies of it in the array. Ex: [1,0] becomes [1,1,1,0,0,0]
+    for digit in fullData:
         horizontal_pixels.extend([digit]*digit_width)
 
     # make an array of pixels for the digits
@@ -18,10 +43,7 @@ def codePrint(digits, filename):
     # invert the pixels because 1 is white and 0 is black
     pixels = [[1 - pixel for pixel in row] for row in pixels]
 
-    # add a bunch of white above and below
-    pixels = [[1]*digit_width*len(digits)]*50 + pixels + [[1]*digit_width*len(digits)]*50
-
-    im = Image.new("1", (digit_width*len(digits),digit_height+100), color=1)
+    im = Image.new("1", (digit_width*len(fullData), digit_height), color=1)
     im.putdata([pixel for row in pixels for pixel in row])
     im.save(filename)
 
