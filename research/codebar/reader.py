7efe54e46def16507f3667289cf8694a468d57fd
TTG3333 <ngassamsimon@gmail.com>
2022-09-26 14:17:27 -0400

    Added progressive inline scanning

--- research/codebar/reader.py
+++ research/codebar/reader.py
@@ -42,100 +42,109 @@ def readCode(imgFilename):
 
     # Insatead of just reading the top row, read the image row by row and print all data found
     for row in pixels:
-        # Measure the width of a bar
-        #  The start of any barcode is 101, so we can measure the width in pixels of the 0, and then take that as the width of a bar
-        blackStart = -1
-        whiteStart = None
-        whiteEnd = None
-        reader = 0
+        blackStarts = []
+        currentBlack = False
+        for index in range(len(row)):
+            if row[index] == 1 and currentBlack == False:
+                blackStarts.append(index)
+                currentBlack = True
+            if row[index] == 0:
+                currentBlack = False
+        for start in blackStarts:
+            # Measure the width of a bar
+            #  The start of any barcode is 101, so we can measure the width in pixels of the 0, and then take that as the width of a bar
+            blackStart = -1
+            whiteStart = None
+            whiteEnd = None
+            reader = start
 
-        doubleBreak = False
-        while whiteEnd == None:
-            if reader >= len(row):
-                doubleBreak = True
-                break
-            if row[reader] == 1 and blackStart == -1:
-                blackStart = reader
-            if blackStart != -1 and row[reader] == 0 and whiteStart == None:
-                whiteStart = reader
+            doubleBreak = False
+            while whiteEnd == None:
+                if reader >= len(row):
+                    doubleBreak = True
+                    break
+                if row[reader] == 1 and blackStart == -1:
+                    blackStart = reader
+                if blackStart != -1 and row[reader] == 0 and whiteStart == None:
+                    whiteStart = reader
+                
+                if blackStart != -1 and row[reader] == 1 and whiteStart != None:
+                    whiteEnd = reader
+
+                reader += 1
+
+            if doubleBreak:
+                # print("Nothing")
+                continue
+
+            barWidth = whiteEnd - whiteStart
+
+            # Read the headers
+            data = []
+
+            currentRegion = 1 # Starts with black
+            regionWidth = 0
+            for pixel in row[blackStart:]: # We'll read until the end, and then only interpret the data we need
+                if pixel == currentRegion:
+                    regionWidth += 1
+                else:
+                    # Write the region to the data
+                    data.extend([currentRegion] * round(regionWidth / barWidth)) # This rounding is to try and get the closest number of bars
+                    currentRegion = pixel
+                    regionWidth = 1
+
+            if len(data) < 18:
+                # Not enough data to be a barcode
+                continue
+
+            # Write the final data
+            data.extend([currentRegion] * round(regionWidth / barWidth))
+
+            dataType = dataTypeReverse[",".join([str(v) for v in data[4:6]])]
             
-            if blackStart != -1 and row[reader] == 1 and whiteStart != None:
-                whiteEnd = reader
+            dataLength = int("".join([str(v) for v in data[6:14]]), 2)
+            if dataLength <= 0:
+                # No data
+                continue
 
-            reader += 1
+            checksumLength = floor(log2(dataLength))+1
 
-        if doubleBreak:
-            #print("Nothing")
-            continue
+            checksum = int("".join([str(v) for v in data[14:14+checksumLength]]), 2)
 
-        barWidth = whiteEnd - whiteStart
+            dataRangeStart = 14+checksumLength
+            dataRangeEnd = dataRangeStart + dataLength * 8
 
-        # Read the headers
-        data = []
+            outdata = data[dataRangeStart:dataRangeEnd]
 
-        currentRegion = 1 # Starts with black
-        regionWidth = 0
-        for pixel in row[blackStart:]: # We'll read until the end, and then only interpret the data we need
-            if pixel == currentRegion:
-                regionWidth += 1
-            else:
-                # Write the region to the data
-                data.extend([currentRegion] * round(regionWidth / barWidth)) # This rounding is to try and get the closest number of bars
-                currentRegion = pixel
-                regionWidth = 1
+        
+            # check that it correctly starts with 1011
+            if data[0:4] != [1,0,1,1]:
+                if data[0:4] == [1,0,1,0]:
+                    if debug: print("Potential backwards reading")
+                    backwards = True
+                else:
+                    if debug: print("invalid start")
+                continue
 
-        if len(data) < 18:
-            # Not enough data to be a barcode
-            continue
+            # check that it correctly ends with 0101.
+            if data[dataRangeEnd:dataRangeEnd + 4] != [0,1,0,1]:
+                if debug: print("Invalid end")
+                continue
+            
+            # check that the checksum is correct
+            checksumValue = 0
+            for i in range(dataLength*8):
+                checksumValue += (i+1)**outdata[i]
+            checksumValue %= 2**checksumLength
 
-        # Write the final data
-        data.extend([currentRegion] * round(regionWidth / barWidth))
+            if checksumValue != checksum:
+                if debug: print("Checksum failed")
+                continue
 
-        dataType = dataTypeReverse[",".join([str(v) for v in data[4:6]])]
-
-        dataLength = int("".join([str(v) for v in data[6:14]]), 2)
-
-        if dataLength <= 0:
-            # No data
-            continue
-
-        checksumLength = floor(log2(dataLength))+1
-
-        checksum = int("".join([str(v) for v in data[14:14+checksumLength]]), 2)
-
-        dataRangeStart = 14+checksumLength
-        dataRangeEnd = dataRangeStart + dataLength * 8
-
-        outdata = data[dataRangeStart:dataRangeEnd]
-
-       
-        # check that it correctly starts with 1011
-        if data[0:4] != [1,0,1,1]:
-            if data[0:4] == [1,0,1,0]:
-                if debug: print("Potential backwards reading")
-                backwards = True
-            else:
-                if debug: print("invalid start")
-            continue
-
-        # check that it correctly ends with 0101.
-        if data[dataRangeEnd:dataRangeEnd + 4] != [0,1,0,1]:
-            if debug: print("Invalid end")
-            continue
-
-        # check that the checksum is correct
-        checksumValue = 0
-        for i in range(dataLength*8):
-            checksumValue += (i+1)**outdata[i]
-        checksumValue %= 2**checksumLength
-
-        if checksumValue != checksum:
-            if debug: print("Checksum failed")
-            continue
-
-        output = decode(outdata)
-        # Conver the data into a string, knowing the data is ascii in binary
-        outputs.add(''.join(output))
+            # Conver the data into a string, knowing the data is ascii in binary
+            output = decode(outdata)
+            outputs.add(''.join(output))
+            break
 
         # Print
         if debug: print("Good data: " + output)
