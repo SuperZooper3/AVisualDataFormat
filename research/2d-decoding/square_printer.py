dfbf56090b82105cf573d22e86378f17f3261c67
SuperZooper3 <54122151+SuperZooper3@users.noreply.github.com>
2022-10-25 23:23:51 -0400

    Better square printer that puts data into the square

--- research/2d-decoding/square_printer.py
+++ research/2d-decoding/square_printer.py
@@ -5,59 +5,52 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import json
 
-# The size of the image
-width = 100
-height = 100
 
-# The array of pixels
-pixels = [[1]*width for i in range(height)]
+def printDataSquare(data, filename, pxSize=1):
+    assert(len(data) == 25)
 
-# K is the number of multiples of the outline to have space inside
-## Ex: K = 3 gives
-##  # # # # #
-##  # . . . #
-##  # . . . #
-##  # . . . #
-##  # # # # #
-K = 5
+    # The size of the image
+    width = (5+4)*pxSize
+    height = (5+4)*pxSize
 
-# X is is the width of the edge
-X = 1
+    # The array of pixels
+    pixels = [[1]*width for i in range(height)]
 
-square_corners = []
+    # Chunk the data (5 bits per chunk)
+    chunks = [data[i:i+5] for i in range(0, len(data), 5)]
 
-# The number of squares to place
-numSquares = 5
+    def writePixel(x, y, value):
+        for i in range(x, x+pxSize):
+            for j in range(y, y+pxSize):
+                pixels[j][i] = value
 
-for i in range(numSquares):
-    # Pick a random top left corner for the square (ensure that it's going to be inside the immage so we limit the range)
-    x = random.randint(0, width - X*(K+2))
-    y = random.randint(0, height - X*(K+2))
+    # Add the bits to the pixels
+    for i in range(len(chunks)):
+        for j in range(len(chunks[i])):
+            writePixel((i+2)*pxSize, (j+2)*pxSize, chunks[i][j])
 
-    square_corners.append((x,y))
+    # Add the bottom of the black square
+    for i in range(len(chunks)+4):
+        writePixel(i*pxSize, 8*pxSize, 0)
 
-    # Place the outline of the square
-    for j in range((K+1)*X): # for each pixel of the edge minus the thicness that'll be drawn by the other strokes
-        for k in range(X): # for each pixel of thickness
-            # draw the top row
-            pixels[y+k][x+j] = 0
-            # draw the bottom row
-            pixels[y+k+(K+1)*X][x+j+X] = 0
-            # draw the left collum
-            pixels[y+j+X][x+k] = 0
-            # draw the right collum
-            pixels[y+j][x+k+(K+1)*X] = 0
-  
+    # Add the top of the black square
+    for i in range(len(chunks)+4):
+        writePixel(i*pxSize, 0, 0)
 
-# Plot the pixels in matplotlib
-# plt.imshow(pixels, cmap='gray')
-# plt.show()
+    # Add the left side of the black square
+    for i in range(len(chunks)+4):
+        writePixel(0, i*pxSize, 0)
 
-# Save the image to printed.png
-im = Image.new("1", (width, height), color=1)
-im.putdata([pixel for row in pixels for pixel in row])
-im.save("printed.png")
+    # Add the right side of the black square
+    for i in range(len(chunks)+4):
+        writePixel((len(chunks)+3)*pxSize, i*pxSize, 0)
 
-# Dump the coordinates of the corners of the squares to a json file for testing
-with open("square_corners.json", "w") as f:
-    json.dump(square_corners, f)
+    # Save the image to printed.png
+    im = Image.new("1", (width, height), color=1)
+    im.putdata([pixel for row in pixels for pixel in row])
+    im.save(filename)
+
+if __name__ == "__main__":
+    # Generate a random 25-bit string
+    data = [random.randint(0, 1) for i in range(25)]
+    printDataSquare(data, "printed.png", pxSize=100)
