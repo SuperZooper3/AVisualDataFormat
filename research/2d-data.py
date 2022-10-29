37c9f9d567076f0ea9f98dfb27476acbd2d9321a
tommcn <tommcn@mcnamer.ca>
2022-10-29 16:53:26 -0400

    cli + readme

--- research/2d-data.py
+++ research/2d-data.py
@@ -4,7 +4,11 @@ import importlib
 print("What do you want to do?")
 print("[1] Encode a message")
 print("[2] Decode a message")
-choice = int(input("Enter your choice: "))
+try:
+    choice = int(input("Enter your choice: "))
+except ValueError:
+    print("Choose a number pls")
+    exit(3)
 
 # Encode a message
 if choice == 1:
@@ -12,17 +16,28 @@ if choice == 1:
     from codebar import encode
 
     msg = input("Enter an integer: ")
-    encoded_msg = encode.encode(int(msg), type="num")
+    try:
+        encoded_msg = encode.encode(int(msg), type="num")
+    except ValueError:
+        print("Not an integer lol")
+        exit(1)
     vmax = sqr_printer.BITS_TOTAL
     # Pad zeroes
     if len(encoded_msg) < vmax:
         encoded_msg = [0] * (vmax - len(encoded_msg)) + encoded_msg
+    else:
+        print(f"Bit limit ({sqr_printer.BITS_TOTAL}) exceeded")
+        exit(2)
 
     path = input("Enter file path: ")
     sqr_printer.printDataSquare(encoded_msg, path, pxSize=20)
     print("Done!")
     print("You can check the file at", path)
 
-if choice == 2:
+elif choice == 2:
     rect_read = importlib.import_module("2d-decoding.rectangle_reader")
     rect_read.main()
+
+
+else:
+    print("Invalid choice")
---
+++ research/README.md
@@ -0,0 +1,29 @@
+# Research
+
+This directory contains our research into data encoding.
+
+`2d-decoding`: Decoding 2D data, including rectangle detection
+`2d-encoding`: TODO
+`codebar`: Decoding 1D barcodes
+`error-correction`: Error correction
+
+In order to run the code, you will need to install the requirements in
+`../requirements.txt`.
+
+Because of the way python packaging works, you will need to run the code from
+this directory or higher. For example see `2d-data.py`.
+
+To import code from directories using dashes or other illegal character, you
+will want to use the following:
+
+```python
+import importlib
+importlib.import_module("2d-decoding")
+```
+
+as opposed to
+
+```python
+import 2d-decoding
+```
+
