1ad5084d548adaf7f8a2e50e5fc450e017d38f28
SuperZooper3 <54122151+SuperZooper3@users.noreply.github.com>
2022-10-29 16:32:46 -0400

    Backup 45deg rotation

--- research/2d-decoding/rectangle_reader.py
+++ research/2d-decoding/rectangle_reader.py
@@ -5,12 +5,35 @@ import cv2
 from .rectangle_detector import readImage
 from .rectangle_deformer import squarifyRectangle
 from .square_decoder import decode_square
+import numpy as np
 
 
-def process(filename):
+def process(filename, tryAgain = True):
     corners = readImage(filename)
     squarifyRectangle(filename, corners)
-    decode_square(directory="2d-decoding/deformed")
+    out_data = decode_square(directory="2d-decoding/deformed")
+    if len(out_data) == 0 and tryAgain:
+        # If data isn't found, mabye the rectangle algorithm failed, try again with a 45 degree rotation
+        # Taken from https://pyimagesearch.com/2017/01/02/rotate-images-correctly-with-opencv-and-python/
+        img = cv2.imread(filename)
+        (h, w) = img.shape[:2]
+        (cX, cY) = (w // 2, h // 2)
+        # grab the rotation matrix (applying the negative of the
+        # angle to rotate clockwise), then grab the sine and cosine
+        # (i.e., the rotation components of the matrix)
+        M = cv2.getRotationMatrix2D((cX, cY), -45, 1.0)
+        cos = np.abs(M[0, 0])
+        sin = np.abs(M[0, 1])
+        # compute the new bounding dimensions of the image
+        nW = int((h * sin) + (w * cos))
+        nH = int((h * cos) + (w * sin))
+        # adjust the rotation matrix to take into account translation
+        M[0, 2] += (nW / 2) - cX
+        M[1, 2] += (nH / 2) - cY
+        # perform the actual rotation and return the image
+        img =  cv2.warpAffine(img, M, (nW, nH))
+        cv2.imwrite("rotated.png", img)
+        process("rotated.png", tryAgain=False)
 
 
 def main():
