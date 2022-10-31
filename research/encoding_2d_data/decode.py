037d23b7fe304db8d895acdc89ad38fcd8a39d85
SuperZooper3 <54122151+SuperZooper3@users.noreply.github.com>
2022-10-30 21:08:41 -0400

    Decode all data / dont crash if there isn't any

--- research/encoding_2d_data/decode.py
+++ research/encoding_2d_data/decode.py
@@ -2,22 +2,24 @@ import os
 
 width = int(os.getenv("QR_BITS_PER_CHUNK", 5))
 
-def decode(bits):
-    bits = bits[0]
+def decode(allBits):
+    output = []
+    for bits in allBits:
+        # Flip bits
+        bits = list(map(lambda x: x[::-1], bits))
+        
+        # Correct rotation
+        while not (bits[0][0] == 1 and bits[0][-1] == 1):
+            bits = list(zip(*bits[::-1]))
 
-    # Flip bits
-    bits = list(map(lambda x: x[::-1], bits))
+        # Remove rotation bits
+        data = [b for row in bits for b in row]
+        data.pop(width**2 - 1)
+        data.pop(width**2 - width)
+        data.pop(width-1)
+        data.pop(0)
+
+        binary = "".join(map(str, data))
+        output.append(int(binary, 2))
     
-    # Correct rotation
-    while not (bits[0][0] == 1 and bits[0][-1] == 1):
-        bits = list(zip(*bits[::-1]))
-
-    # Remove rotation bits
-    data = [b for row in bits for b in row]
-    data.pop(width**2 - 1)
-    data.pop(width**2 - width)
-    data.pop(width-1)
-    data.pop(0)
-
-    binary = "".join(map(str, data))
-    return int(binary, 2)
+    return output
